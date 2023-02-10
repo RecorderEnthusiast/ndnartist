fd03bd00f54dcbffd869945a9cb7c5614c46961c
RecorderEnthusiast <85328186+RecorderEnthusiast@users.noreply.github.com>
2023-02-10 00:42:59 -0600

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,60 +1,57 @@
-var gallery_images; 
+var gallery_images = document.getElementsByClassName("grid-item");
 var gallery_image_index = 0;
 
-var modal; 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var modalImg;
-var captionText;
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+var is_modal_active = false;
 
-var span; 
-var selected_image;
-
-window.onload = function() {
-    //showSlides(slideIndex);
-    gallery_images = document.getElementsByClassName("grid-item");
-    gallery_image_index = 0;
-
-    modal = document.getElementById("myModal");
-    modalImg = document.getElementById("img01");
-    captionText = document.getElementById("caption");
-    span = document.getElementsByClassName("close")[0];
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-
-    $('img').click(function() {
-        //this info is used in another event
+//== EVENTS ==
+$('.grid-item').click(function() {
+    //if the modal is active don't allow anyone to update it
+    if ( is_modal_active == false ) {
+        //@NOTE: this a bug but this code will work as bandaid
         gallery_image_index = $(this).data('index');
         Update_Modal(gallery_image_index);
-
-        //keeping incase I need for other elements in the future
-        //console.log($thumb.data('foo'));
-        //console.log($thumb.find('img').attr('src'));
-    });
-
-    //@NOTE: do not inline the function call, you need to wrap this in an anonymous function call why? who knows
-    $('.next').click(function() {Update_Modal(gallery_image_index+1);});
-    $('.prev').click(function() {Update_Modal(gallery_image_index-1);});
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
+        is_modal_active = true;
     }
-}
+    //this info is used in another event
+
+    //keeping incase I need for other elements in the future
+    //console.log($thumb.data('foo'));
+    //console.log($thumb.find('img').attr('src'));
+});
+
+//@NOTE: do not inline the function call, you need to wrap this in an anonymous function call why? who knows
+$('.next').click(function() {Update_Modal(gallery_image_index+1);});
+$('.prev').click(function() {Update_Modal(gallery_image_index-1);});
+
+$('#close-button').click(function() {
+    modal.style.display = "none";
+    gallery_image_index = 0;
+    is_modal_active = false;
+});
 
 function Update_Modal(selected_image_index) {
+    if ( selected_image_index === undefined ) {
+        return;
+    }
 
     console.log("update_modal");
-if (selected_image_index >= gallery_images.length) {
-    selected_image_index = 0;
-} else if (selected_image_index < 0) {
-    selected_image_index = gallery_images.length-1;
-}
+    if (selected_image_index >= gallery_images.length) {
+        selected_image_index = 0;
+    }
+    else if (selected_image_index < 0) {
+        selected_image_index = gallery_images.length-1;
+    }
+
     //@NOTE: the $(...) is special jquery syntax without it you won't be able to call .attr();
-    if (selected_image_index === undefined || selected_image_index >= gallery_images.length) {
-  return;
-}
-	let selected_image = $(gallery_images[selected_image_index]);
+    let selected_image = $(gallery_images[selected_image_index]);
+
     modal.style.display = "block";
     modalImg.src = selected_image.attr('src');
     captionText.innerHTML = selected_image.attr('alt');
+
     gallery_image_index = selected_image_index;
 }
