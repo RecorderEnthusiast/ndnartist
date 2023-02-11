3537f54e4ec7ee3796b7b34e31d9f3d2a99948ae
RecorderEnthusiast <85328186+RecorderEnthusiast@users.noreply.github.com>
2023-02-10 23:11:16 -0600

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+const scrollToTopBtn = document.getElementById("scroll-to-top");
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
